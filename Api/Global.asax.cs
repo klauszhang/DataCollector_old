3bc86972393d48b82b71bbc0b4295e789f61d72f
Hao Zhang <klaushaozhang.note@gmail.com>
2015-12-18 18:57:47 +1300

    enable camel case for json objects

--- Api/Global.asax.cs
+++ Api/Global.asax.cs
@@ -4,6 +4,8 @@ using System.Linq;
 using System.Web;
 using System.Web.Http;
 using System.Web.Routing;
+using Newtonsoft.Json;
+using Newtonsoft.Json.Serialization;
 
 namespace Api
 {
@@ -11,6 +13,13 @@ namespace Api
     {
         protected void Application_Start()
         {
+            #region  enable camel case json
+            var formatters = GlobalConfiguration.Configuration.Formatters;
+            var jsonFormatter = formatters.JsonFormatter;
+            var settings = jsonFormatter.SerializerSettings;
+            settings.Formatting=Formatting.Indented;
+            settings.ContractResolver=new CamelCasePropertyNamesContractResolver();
+            #endregion
             GlobalConfiguration.Configure(WebApiConfig.Register);
         }
     }
